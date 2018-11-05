49ba6b1a7783ab938f83f56de782d8c24edac24f
marton987 <martin.jfreytes@gmail.com>
2018-11-05 10:31:03 +0000

    Delete hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,6 +21,11 @@ export class HeroesComponent implements OnInit {
       .subscribe(hero => this.heroes.push(hero));
   }
 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
